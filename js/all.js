d3b8cce293929ee4d69b19d9e02cef8b55f7a49f
Vlad <24voltage24@gmail.com>
2024-02-13 09:28:19 +0300

    new

--- js/all.js
+++ js/all.js
@@ -1,13 +1,33 @@
-$(function() {
-  var form_title;
+let msquare = document.getElementById('meters')
+let floorHeight = document.getElementById('height')
+let calcPrice = document.getElementById('calcPrice')
 
-  $( ".premises .uk-button" ).click(function() {
-    form_title = $(this).closest('.wrapper').children('h3').text()
-    $('#modal-call input[name=modalinfo]').val('Помещение:' + form_title);
-  });
+document.addEventListener("input", EventSelector)
+function EventSelector(ev) {
+  console.log(ev.target.id);
+  switch (ev.target.id) {
+    case 'meters': {calculator(); break}
+    case 'height': {calculator(); break}
+  }
+}
 
-  $( ".results .uk-button" ).click(function() {
-    form_title = $(this).closest('.info').find('h3').text()
-    $('#modal-call input[name=modalinfo]').val(form_title);
-  }); 
-});
+function calculator() {
+  let msquareP = parseInt(msquare.value)
+  let floorHeightP = parseInt(floorHeight.value)
+  if (msquareP > 5000) {msquare.value = 5000; calculator(); return;}
+  if (floorHeightP > 80) {floorHeight.value = 80; calculator(); return;}
+  if (isNaN(msquareP) || isNaN(floorHeightP)) {return;}
+  if (msquareP < 0) {msquare.value = 1; calculator(); return;}
+  if (floorHeightP < 8) {floorHeight.value = 8; calculator(); return;}
+  let result = 0
+
+    if (msquareP <= 0 || floorHeightP <= 0) {calcPrice.innerText = ''; return}
+    console.log(msquareP, floorHeightP)
+    let workPrice = msquareP >= 51 ? 1000:1200
+    result = workPrice * msquareP + floorHeightP * 3.20 * msquareP
+    calcPrice.innerText = result.toString()
+
+  // else {
+  //   // calcPrice.innerText = 'Введите значение'
+  // }
+}
